9494dd51f6013ad66bfd2be46436713a94927b33
fusion <fusion@localhost.localdomain>
2024-05-08 18:53:34 +0700

    update

--- todos/src/app/modules/home/pages/home/home.component.ts
+++ todos/src/app/modules/home/pages/home/home.component.ts
@@ -1,38 +1,74 @@
-import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription, delay, from, interval, of, timer } from 'rxjs';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import {
+  BehaviorSubject,
+  Observable,
+  Subject,
+  Subscription,
+  combineLatest,
+  combineLatestWith,
+  concatMap,
+  delay,
+  from,
+  fromEvent,
+  interval,
+  map,
+  merge,
+  mergeMap,
+  of,
+  switchMap,
+  take,
+  timer,
+} from 'rxjs';
+import { PictureService } from '../../../../core/http/picture/picture.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit, OnDestroy {
+  @Input() name = '';
 
-  @Input() name = ''
+  number$: Observable<number>;
 
-  number$: Observable<number>
+  nums: number[] = [];
 
-  nums: number[] = []
+  subcription: any;
 
-  subcription: any
-
-  constructor() {
-    this.number$ = interval(1000)
+  constructor(private pictureService: PictureService) {
+    this.number$ = interval(1000).pipe(take(5));
   }
   ngOnDestroy(): void {
-    this.subcription.unsubscribe()
+    this.subcription.unsubscribe();
   }
 
   changeName(event: Event) {
-    this.name = (event.target as HTMLInputElement).value
+    this.name = (event.target as HTMLInputElement).value;
   }
+
+  subject = new BehaviorSubject('0');
 
   ngOnInit(): void {
-    this.subcription = this.number$.subscribe(num => {
-      console.log(num)
-      this.nums.push(num)
-    }) 
-  }
+    this.subcription = this.number$.subscribe((num) => {
+      // console.log(num)
+      this.nums.push(num);
+    });
+    this.subject.subscribe((value) => console.log('first', value));
+    this.subject.next('a');
+    this.subject.subscribe((value) => console.log('second', value));
+    this.subject.next('b');
 
+    // this.pictureService.getOneImage('2').pipe(
+    //   combineLatestWith(this.pictureService.getOneImage('1').pipe(delay(2000)))
+    // ).subscribe(console.log)
+
+    // interval(5000).pipe(take(10), concatMap(() => interval(1000).pipe(take(10)))).subscribe(console.log)
+  }
 }
